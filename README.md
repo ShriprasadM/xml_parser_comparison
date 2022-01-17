3d66de4b0afeecfb0ba0f138df3701c526e04caa
dhruv.sonone <dhruv.sonone@pubmatic.com>
2022-01-17 13:34:24 +0530

    Multiple Creative

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 
 
 ```s
-Running tool: /usr/local/go/bin/go test -benchmem -run=^$ -coverprofile=/var/folders/_q/40r7ttwx7gvcgy_nv46hz0x40000gp/T/vscode-goQUY6HW/go-code-cover -bench . xml_parser_comparison
+Running tool: /usr/local/bin/go test -benchmem -run=^$ -coverprofile=/var/folders/z5/cj4zjtv15wn8yt53qzh57lnr0000gp/T/vscode-goj9wKIi/go-code-cover -bench . xml_parser_comparison
 
 goos: darwin
 goarch: amd64
 pkg: xml_parser_comparison
-cpu: Intel(R) Core(TM) i5-8259U CPU @ 2.30GHz
-BenchmarkStringBased-8        	  504448	      2377 ns/op	    3072 B/op	       1 allocs/op
-BenchmarkEtreeBased-8         	   10000	    115355 ns/op	   47504 B/op	     659 allocs/op
-BenchmarkXmlEncodingBased-8   	   10000	    117195 ns/op	   37812 B/op	     589 allocs/op
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+BenchmarkStringBased-12         	  141146	      7661 ns/op	   12352 B/op	       5 allocs/op
+BenchmarkEtreeBased-12          	    5790	    206467 ns/op	   93800 B/op	    1323 allocs/op
+BenchmarkXmlEncodingBased-12    	    6192	    195523 ns/op	   75089 B/op	    1105 allocs/op
 PASS
-coverage: 71.7% of statements
-ok  	xml_parser_comparison	3.589s
+coverage: 78.7% of statements
+ok  	xml_parser_comparison	3.849s
 ```
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/beevik/etree"
@@ -17,28 +18,66 @@ const (
 	trackingEventsTagCloseTag = "</" + trackingEventsTag + ">"
 	SampleTrackingEvent       = "<Tracking event=\"shriprasad\"><![CDATA[https://mytracker.com]]></Tracking>"
 	linearEndTag              = "</Linear>"
+	nonLinearEndTag           = "</NonLinearAds>"
 	trackerUrl                = "https://mytracker.com"
 )
 
-func stringBased(vast string) (string, error) {
-	ci := strings.Index(vast, trackingEventsTagCloseTag)
+// func stringBased(vast string) (string, error) {
+// 	ci := strings.Index(vast, trackingEventsTagCloseTag)
 
-	// no impression tag - pass it as it is
-	if ci == -1 {
-		// return vast, nil
-		// append tracking event
-		// oi := strings.Index(vast, trackingEventsTagOpenTag)
+// 	// no impression tag - pass it as it is
+// 	if ci == -1 {
+// 		// return vast, nil
+// 		// append tracking event
+// 		// oi := strings.Index(vast, trackingEventsTagOpenTag)
 
-		oi := strings.Index(vast, linearEndTag)
+// 		oi := strings.Index(vast, linearEndTag)
 
-		// if ci-oi == len(trackingEventsTagOpenTag) {
-		if oi != -1 {
-			return strings.Replace(vast, linearEndTag, trackingEventsTagOpenTag+SampleTrackingEvent+trackingEventsTagCloseTag+linearEndTag, 1), nil
+// 		// if ci-oi == len(trackingEventsTagOpenTag) {
+// 		if oi != -1 {
+// 			return strings.Replace(vast, linearEndTag, trackingEventsTagOpenTag+SampleTrackingEvent+trackingEventsTagCloseTag+linearEndTag, 1), nil
+// 		}
+// 		return vast, nil // single replacement
+// 	}
+
+// 	return strings.Replace(vast, trackingEventsTagCloseTag, SampleTrackingEvent+trackingEventsTagCloseTag, 1), nil
+// }
+
+func stringBased(vast string) string {
+
+	list := strings.SplitAfter(vast, "/Creative>")
+
+	for i, cr := range list {
+		ci := strings.Index(cr, trackingEventsTagCloseTag)
+
+		// // no impression tag - pass it as it is
+		if ci == -1 {
+			// 	// return vast, nil
+			// 	// append tracking event
+			// 	// oi := strings.Index(vast, trackingEventsTagOpenTag)
+
+			// 	oi := strings.Index(cr, linearEndTag)
+
+			// 	// if ci-oi == len(trackingEventsTagOpenTag) {
+			// 	if oi != -1 {
+			// 		list[i] = strings.Replace(cr, linearEndTag, trackingEventsTagOpenTag+SampleTrackingEvent+trackingEventsTagCloseTag+linearEndTag, 1)
+			// 	}
+			// }
+
+			li := strings.Index(cr, linearEndTag)
+			if li != -1 {
+				list[i] = strings.Replace(cr, linearEndTag, trackingEventsTagOpenTag+SampleTrackingEvent+trackingEventsTagCloseTag+linearEndTag, 1)
+			}
+
+			nli := strings.Index(cr, nonLinearEndTag)
+			if nli != -1 {
+				list[i] = strings.Replace(cr, nonLinearEndTag, trackingEventsTagOpenTag+SampleTrackingEvent+trackingEventsTagCloseTag+nonLinearEndTag, 1)
+			}
 		}
-		return vast, nil // single replacement
+		list[i] = strings.Replace(cr, trackingEventsTagCloseTag, SampleTrackingEvent+trackingEventsTagCloseTag, 1)
 	}
 
-	return strings.Replace(vast, trackingEventsTagCloseTag, SampleTrackingEvent+trackingEventsTagCloseTag, 1), nil
+	return strings.Join(list, "")
 }
 
 func etreeBased(vast string) (string, error) {
@@ -47,13 +86,25 @@ func etreeBased(vast string) (string, error) {
 		panic(err)
 	}
 
-	// ele := doc.SelectElement("impressionTag")
+	for i, ele := range doc.FindElements("VAST/Ad/InLine/Creatives/Creative") {
+		trackEle := ele.FindElement("Linear/" + trackingEventsTag)
+		if trackEle != nil {
+			newEle := doc.CreateElement("Tracking")
+			newEle.CreateAttr("event", "shriprasad_"+strconv.Itoa(i))
+			newEle.SetCData(trackerUrl)
+			trackEle.AddChild(newEle)
+		}
 
-	ele := doc.FindElement("VAST/Ad/InLine/Creatives/Creative/Linear/" + trackingEventsTag)
-	newEle := doc.CreateElement("Tracking")
-	newEle.CreateAttr("event", "shriprasad")
-	newEle.SetCData(trackerUrl)
-	ele.AddChild(newEle)
+		trackEle = ele.FindElement("NonLinearAds/" + trackingEventsTag)
+		if trackEle != nil {
+			newEle := doc.CreateElement("Tracking")
+			newEle.CreateAttr("event", "shriprasad_"+strconv.Itoa(i))
+			newEle.SetCData(trackerUrl)
+			trackEle.AddChild(newEle)
+		}
+
+	}
+
 	return doc.WriteToString()
 }
 
@@ -137,3 +188,106 @@ func xmlEncodingBased(vast string) (string, error) {
 
 	return buf.String(), nil
 }
+
+func main() {
+	vast := stringBased(vast)
+	//vast, _ := xmlEncodingBased(vast)
+	fmt.Println(vast)
+}
+
+var vast string = `
+<VAST version="4.2" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns="http://www.iab.com/VAST">
+  <Ad id="20001" >
+    <InLine>
+      <AdSystem version="1">iabtechlab</AdSystem>
+      <Error><![CDATA[https://example.com/error]]></Error>
+      <Impression id="Impression-ID"><![CDATA[https://example.com/track/impression]]></Impression>
+      <AdServingId>a532d16d-4d7f-4440-bd29-2ec05553fc80</AdServingId>
+      <AdTitle>Inline Simple Ad</AdTitle>
+      <AdVerifications></AdVerifications>
+      <Advertiser>IAB Sample Company</Advertiser>
+      <Category authority="https://www.iabtechlab.com/categoryauthority">AD CONTENT description category</Category>
+      <Creatives>
+        <Creative id="5480" sequence="1" adId="2447226">
+          <Linear>
+            <TrackingEvents>
+              <Tracking event="start" ><![CDATA[https://example.com/tracking/start]]></Tracking>
+              <Tracking event="progress" offset="00:00:10"><![CDATA[http://example.com/tracking/progress-10]]></Tracking>
+              <Tracking event="firstQuartile"><![CDATA[https://example.com/tracking/firstQuartile]]></Tracking>
+              <Tracking event="midpoint"><![CDATA[https://example.com/tracking/midpoint]]></Tracking>
+              <Tracking event="thirdQuartile"><![CDATA[https://example.com/tracking/thirdQuartile]]></Tracking>
+              <Tracking event="complete"><![CDATA[https://example.com/tracking/complete]]></Tracking>
+            </TrackingEvents>
+            <Duration>00:00:16</Duration>
+            <MediaFiles>
+              <MediaFile id="5241" delivery="progressive" type="video/mp4" bitrate="2000" width="1280" height="720" minBitrate="1500" maxBitrate="2500" scalable="1" maintainAspectRatio="1" codec="H.264">
+                <![CDATA[https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro.mp4]]>
+              </MediaFile>
+              <MediaFile id="5244" delivery="progressive" type="video/mp4" bitrate="1000" width="854" height="480" minBitrate="700" maxBitrate="1500" scalable="1" maintainAspectRatio="1" codec="H.264">
+                <![CDATA[https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro-mid-resolution.mp4]]>
+              </MediaFile>
+              <MediaFile id="5246" delivery="progressive" type="video/mp4" bitrate="600" width="640" height="360" minBitrate="500" maxBitrate="700" scalable="1" maintainAspectRatio="1" codec="H.264">
+                <![CDATA[https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro-low-resolution.mp4]]>
+              </MediaFile>
+            </MediaFiles>
+            <VideoClicks>
+              <ClickThrough id="blog">
+                <![CDATA[https://iabtechlab.com]]>
+              </ClickThrough>
+            </VideoClicks>
+          </Linear>
+          <UniversalAdId idRegistry="Ad-ID">8465</UniversalAdId>
+        </Creative>
+		<Creative id="5480" sequence="1" adId="2447226">
+          <Linear>
+            <TrackingEvents>
+              <Tracking event="start" ><![CDATA[https://example.com/tracking/start]]></Tracking>
+              <Tracking event="progress" offset="00:00:10"><![CDATA[http://example.com/tracking/progress-10]]></Tracking>
+              <Tracking event="firstQuartile"><![CDATA[https://example.com/tracking/firstQuartile]]></Tracking>
+              <Tracking event="midpoint"><![CDATA[https://example.com/tracking/midpoint]]></Tracking>
+              <Tracking event="thirdQuartile"><![CDATA[https://example.com/tracking/thirdQuartile]]></Tracking>
+              <Tracking event="complete"><![CDATA[https://example.com/tracking/complete]]></Tracking>
+            </TrackingEvents>
+            <Duration>00:00:16</Duration>
+            <MediaFiles>
+              <MediaFile id="5241" delivery="progressive" type="video/mp4" bitrate="2000" width="1280" height="720" minBitrate="1500" maxBitrate="2500" scalable="1" maintainAspectRatio="1" codec="H.264">
+                <![CDATA[https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro.mp4]]>
+              </MediaFile>
+              <MediaFile id="5244" delivery="progressive" type="video/mp4" bitrate="1000" width="854" height="480" minBitrate="700" maxBitrate="1500" scalable="1" maintainAspectRatio="1" codec="H.264">
+                <![CDATA[https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro-mid-resolution.mp4]]>
+              </MediaFile>
+              <MediaFile id="5246" delivery="progressive" type="video/mp4" bitrate="600" width="640" height="360" minBitrate="500" maxBitrate="700" scalable="1" maintainAspectRatio="1" codec="H.264">
+                <![CDATA[https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro-low-resolution.mp4]]>
+              </MediaFile>
+            </MediaFiles>
+            <VideoClicks>
+              <ClickThrough id="blog">
+                <![CDATA[https://iabtechlab.com]]>
+              </ClickThrough>
+            </VideoClicks>
+          </Linear>
+          <UniversalAdId idRegistry="Ad-ID">8465</UniversalAdId>
+        </Creative>
+		<Creative id="5480" sequence="1" adId="2447226">
+		  <UniversalAdId idRegistry="Ad-ID" idValue="8465">8465</UniversalAdId>
+		  <NonLinearAds>
+		    <NonLinear>
+		    <StaticResource creativeType="image/png">
+		    <![CDATA[http://mms.businesswire.com/media/20150623005446/en/473787/21/iab_tech_lab.jpg]]>
+		    </StaticResource>
+		    <NonLinearClickThrough>
+		    <![CDATA[http://iabtechlab.com]]>
+		    </NonLinearClickThrough>
+		    <NonLinearClickTracking>
+		    <![CDATA[http://example.com/trackingurl/clickTracking]]>
+		    </NonLinearClickTracking>
+		   </NonLinear>
+		   <TrackingEvents>
+	  	   </TrackingEvents>
+	  	  </NonLinearAds>
+		</Creative>
+      </Creatives>
+    </InLine>
+  </Ad>
+</VAST>
+`
